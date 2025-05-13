153fda6eeed072c5b1c25f6c00391cfd250769d9
Pepega-1984 <alexandr.minyaev@gmail.com>
2025-05-13 17:43:09 +0300

    Create CardmanagementApplicationTests.java

---
+++ src/test/java/com/example/cardmanagement/CardmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cardmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
